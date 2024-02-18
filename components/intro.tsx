1d3f0e125e48fc8bf7c4058c171bfeb21d90a600
Faraz <shahfarazulhaq@gmail.com>
2024-02-18 16:01:53 -0500

    new changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -27,7 +27,7 @@ export default function Intro() {
 						animate={{ opacity: 1, scale: 1 }}
 						transition={{
 							type: 'tween',
-							duration: 0.2,
+							duration: 0.3,
 						}}
 					>
 						<Image
