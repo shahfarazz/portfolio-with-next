719232c7e854bcca2393cafec6d5431a391f4723
Faraz <shahfarazulhaq@gmail.com>
2024-02-18 15:56:27 -0500

    personal-portfolio first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,51 @@
-import Header from "@/components/header";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import ActiveSectionContextProvider from "@/context/active-section-context";
-import Footer from "@/components/footer";
-import ThemeSwitch from "@/components/theme-switch";
-import ThemeContextProvider from "@/context/theme-context";
-import { Toaster } from "react-hot-toast";
+import Header from '@/components/header';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import Footer from '@/components/footer';
+import ThemeSwitch from '@/components/theme-switch';
+import ThemeContextProvider from '@/context/theme-context';
+import { Toaster } from 'react-hot-toast';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Aliva Priyadarshini | Personal Portfolio",
-  description: "Aliva Priyadarshini is a full-stack developer with 3 years of experience.",
+	title: 'Faraz Shah | Personal Portfolio',
+	description:
+		'Faraz UlHaq Shah is a full-stack developer with 2 years of experience.',
+	image: 'https://media.licdn.com/dms/image/D4D35AQH6UFwU_IjGdA/profile-framedphoto-shrink_400_400/0/1692546481662?e=1708891200&v=beta&t=oTFkizM_SWVvnKfiw8OjMzeCpkMJkCOqWhiESZfddew',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
-      >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+	return (
+		<html lang="en" className="!scroll-smooth">
+			<link
+				rel="icon"
+				href="https://media.licdn.com/dms/image/D4D35AQH6UFwU_IjGdA/profile-framedphoto-shrink_400_400/0/1692546481662?e=1708891200&v=beta&t=oTFkizM_SWVvnKfiw8OjMzeCpkMJkCOqWhiESZfddew"
+				type="image/png"
+			/>
+			<body
+				className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+			>
+				<div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#ebabd8]"></div>
+				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#785696]"></div>
 
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            <Header />
-            {children}
-            <Footer />
+				<ThemeContextProvider>
+					<ActiveSectionContextProvider>
+						<Header />
 
-            <Toaster position="top-right" />
-            <ThemeSwitch />
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
-      </body>
-    </html>
-  );
+						{children}
+						<Footer />
+
+						<Toaster position="top-right" />
+						<ThemeSwitch />
+					</ActiveSectionContextProvider>
+				</ThemeContextProvider>
+			</body>
+		</html>
+	);
 }
--- components/about.tsx
+++ components/about.tsx
@@ -1,50 +1,55 @@
-"use client";
+'use client';
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
+import React from 'react';
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+	const { ref } = useSectionInView('About');
 
-  return (
-    <motion.section
-      ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-      id="about"
-    >
-      <SectionHeading>About me ✨</SectionHeading>
-      <p className="mb-3">
-        After earning my degree in{" "}
-        <span className="font-medium">Computer Science</span>, I embarked on a
-        journey fueled by my passion for programming. 🚀{" "}
-        <span className="italic">Solving complex problems</span> is the
-        heartbeat of my work, and the joy of finding solutions is my constant
-        motivation. I stay at the forefront of technology, always eager to
-        explore the latest frameworks and methodologies. My expertise spans a
-        range of technologies, including{" "}
-        <span className="font-medium">
-          JavaScript, React, Next.js, Firebase, Tailwind CSS, and Rest APIs
-        </span>
-        . Additionally, I'm well-versed in TypeScript. My learning journey
-        extends to the backend, where I'm currently diving into{" "}
-        <span className="font-medium">MongoDB, Express, and Node.js</span>.
-        {/* Open to new challenges and learning opportunities, I am */}
-        {/* currently seeking ways to contribute as a{" "} */}
-        {/* <span className="font-medium">full-time software developer</span>. */}
-      </p>
+	return (
+		<motion.section
+			ref={ref}
+			className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+			initial={{ opacity: 0, y: 100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.175 }}
+			id="about"
+		>
+			<SectionHeading>About me 🤙</SectionHeading>
+			<>
+				<p className="mb-3">
+					After earning my degree in{' '}
+					<span className="font-medium">Computer Science</span>, I
+					blend academic prowess with practical experience. 🚀 With a
+					Bachelor of Sciences from Lahore, Pakistan, I've cultivated
+					a strong foundation, excelling in courses such as Software
+					Engineering, Databases, and Human Computer Interaction.
+				</p>
+				<p className="mb-3">
+					My technical toolkit spans diverse languages like Python,
+					Java, and JavaScript, along with proficiency in frameworks
+					like ReactJS and AngularJS. In professional roles at
+					Unilever Karachi and Clariba SEIDOR, I honed my skills in
+					digital transformation and collaborative research, driving
+					impactful projects like ROLL and a fraud detection pipeline
+					for clients in Iraq.
+				</p>
+				<p className="mb-3">
+					My commitment to excellence has earned me accolades like the
+					Deans Honors List Award, reflecting my dedication to
+					academic and professional success.
+				</p>
+			</>
 
-      <p>
-        <span className="italic">Away from the code editor</span>, I find joy in
-        the rhythm of a badminton match, the magic of cinema 🎬, the melody of
-        music 🎵, and the enchantment of a good book 📚. The pursuit of
-        knowledge is a constant companion, and I cherish the thrill of learning
-        new things. 🌱
-      </p>
-    </motion.section>
-  );
+			<p>
+				<span className="italic">Away from the code editor</span>, I
+				find joy in soccer matches, cinematic marvels 🎬, harmonious
+				melodies 🎵, culinary explorations, and the simple joys of being
+				a food enthusiast. The pursuit of knowledge is my guiding light,
+				always propelling me towards new horizons. 🌱
+			</p>
+		</motion.section>
+	);
 }
